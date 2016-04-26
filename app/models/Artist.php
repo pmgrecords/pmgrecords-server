9805ecd08aa787eb872782345dac8a04ffdd39ed
Jonathan R <jreyes.satx@gmail.com>
2016-04-26 10:29:16 -0500

    update models with fillable fields

--- app/models/Artist.php
+++ app/models/Artist.php
@@ -2,7 +2,14 @@
 
 class Artist extends BaseModel {
 
-    protected $fillable = [];
-    
+    protected $fillable = ['order', 'name', 'sound_cloud_url', 'spund_cloud_id'];
+
     protected $hidden = array('created_at', 'updated_at');
+
+    public static $rules = array(
+        'order' => 'required',
+        'name' => 'required',
+        'sound_cloud_url' => 'required',
+        'sound_cloud_id' => 'required',
+    );
 }
--- app/models/Video.php
+++ app/models/Video.php
@@ -1,7 +1,12 @@
 <?php
 
 class Video extends BaseModel {
-	protected $fillable = [];
+	protected $fillable = ['order', 'url'];
 
     protected $hidden = array('created_at', 'updated_at');
+
+    public static $rules = array(
+        'order' => 'required',
+        'url' => 'required',
+    );
 }
